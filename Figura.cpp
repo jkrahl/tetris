8203bcc6069fdf8bbb204a1045a0572e4a0d3238
Jan <jan@krahl.dev>
2024-04-18 18:25:42 +0200

    Matrix inicialització

--- Figura.cpp
+++ Figura.cpp
@@ -1,15 +1,92 @@
 #include "Figura.h"
 
-void Figura::inicialitzaFigura()
+Figura::Figura(TipusFigura tipus, ColorFigura color)
 {
-	if (m_matriuFigura != nullptr)
+	int nCostats = -1;
+	switch (tipus)
 	{
-		for (int i = 0; i < m_nCostats; i++)
+	case FIGURA_J:
+	case FIGURA_L:
+	case FIGURA_T:
+	case FIGURA_S:
+	case FIGURA_Z:
+		nCostats = 3;
+		break;
+	case FIGURA_I:
+		nCostats = 4;
+		break;
+	case FIGURA_O:
+		nCostats = 2;
+		break;
+	default:
+		break;
+	}
+
+	m_nCostats = nCostats;
+	m_color = color;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
 		{
-			for (int j = 0; j < m_nCostats; j++)
-			{
-				m_matriuFigura[i][j] = 0;
-			}
+			m_matriu[i][j] = false;
 		}
+	}
+
+	inicialitzarMatriu();
+}
+
+bool Figura::transposarMatriuFigura()
+{
+	// To do
+	return true;
+}
+
+void Figura::inicialitzarMatriu()
+{
+	switch (m_tipus)
+	{
+	case FIGURA_I:
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		m_matriu[1][2] = true;
+		m_matriu[1][3] = true;
+		break;
+	case FIGURA_J:
+		m_matriu[0][0] = true;
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		m_matriu[1][2] = true;
+		break;
+	case FIGURA_L:
+		m_matriu[0][2] = true;
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		m_matriu[1][2] = true;
+		break;
+	case FIGURA_O:
+		m_matriu[0][0] = true;
+		m_matriu[0][1] = true;
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		break;
+	case FIGURA_S:
+		m_matriu[0][1] = true;
+		m_matriu[0][2] = true;
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		break;
+	case FIGURA_T:
+		m_matriu[0][1] = true;
+		m_matriu[1][0] = true;
+		m_matriu[1][1] = true;
+		m_matriu[1][2] = true;
+	case FIGURA_Z:
+		m_matriu[0][0] = true;
+		m_matriu[0][1] = true;
+		m_matriu[1][1] = true;
+		m_matriu[1][2] = true;
+	default:
+		break;
 	}
 }
--- Figura.h
+++ Figura.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+using namespace std;
 
 typedef enum {
 	COLOR_NEGRE = 0,
@@ -25,28 +26,28 @@ typedef enum {
 } TipusFigura;
 
 typedef enum { GIR_HORARI = 0, GIR_ANTI_HORARI } DireccioGir;
-
+// Ha de servir per poder gestionar figures que van caient.
+// Ha de poder guardar tota la informació necessària per gestionar la caiguda de la figura: tipus i color de la figura, posició actual, forma actual de la figura.
 class Figura
 {
 public:
-	Figura() : m_tipus(NO_FIGURA), m_color(NO_COLOR), m_nCostats(0), m_matriuFigura(nullptr) {}
-	Figura(TipusFigura tipus, ColorFigura color) : m_tipus(tipus), m_color(color), m_nCostats(0), m_matriuFigura(nullptr) {}
+	Figura() : m_tipus(NO_FIGURA), m_color(NO_COLOR), m_nCostats(0) {}
+	Figura(TipusFigura tipus, ColorFigura color);
 
 	TipusFigura getTipus() const { return m_tipus; }
 	ColorFigura getColor() const { return m_color; }
 	int getNCostats() const { return m_nCostats; }
 
-	void inicialitzaFigura();
-	void allibera();
 	bool transposarMatriuFigura();
 	bool invertir(const bool &columnes, const bool &files);
 private:
+	void inicialitzarMatriu();
 	TipusFigura m_tipus;
 	ColorFigura m_color;
 	// nCostats canvia segons tipus de figura
 	int m_nCostats;
 	// matriuFigura es un punter a una matriu de booleans que representa la figura.
 	// La dimensió de la matriu depen del tipus de figura.
-	bool* m_matriuFigura;
+	bool m_matriu[4][4];
 };
 
