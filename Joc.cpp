a5736b648452caf81633555cb3b6e3a60b36c27d
1711569 <1711569@uab.cat>
2024-04-19 09:25:43 +0200

    Commit Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -1,20 +1,86 @@
+#include <iostream>
+#include <fstream>
 #include "Joc.h"
+
+const int MAX_FILES = 8;
+const int MAX_COLUMNES = 8;
 
 void Joc::inicialitza(const string& nomFitxer)
 {
-	// To do
+    ifstream fitxer;
+    fitxer.open(nomFitxer);
+
+    int tipus = 0, x = 0, y = 0, gir = 0;
+
+    if (fitxer.is_open())
+    {
+        fitxer >> tipus >> x >> y >> gir;
+        m_figura_actual.setTipus(static_cast<TipusFigura>(tipus));
+        m_figura_actual.setPosicio(x, y);
+        m_figura_actual.setGir(static_cast<DireccioGir>(gir));
+
+        for (int i = 0; i < MAX_FILES; i++)
+        {
+            for (int j = 0; j < MAX_COLUMNES; j++)
+            {
+                int pixel;
+                fitxer >> pixel;
+                m_tauler.setPixel(i, j, pixel);
+            }
+        }
+    }
+
+    fitxer.close();
 }
+
 
 bool Joc::giraFigura(DireccioGir direccio)
 {
-    // To do
-    return false;
+    bool si_girar = true;
+
+    if (m_figura_actual.getTipus() != FIGURA_O)
+    {
+        if (direccio == GIR_HORARI) //
+        {
+            m_figura_actual.transposarMatriuFigura();
+            m_figura_actual.invertir(true, false);
+        }
+        else
+        {
+            m_figura_actual.transposarMatriuFigura();
+            m_figura_actual.invertir(false, true);
+        }
+
+        for (int i = 0; i < m_figura_actual.getNCostats(); i++)
+        {
+            for (int j = 0; j < m_figura_actual.getNCostats(); j++)
+            {
+                if (m_figura_actual.getPixel(i, j) != COLOR_NEGRE &&
+                    m_tauler.getPixel(i + m_figura.getX(), j + m_figura.getY()))
+                {
+                    if (direccio == GIR_HORARI)
+                    {
+                        m_figura_actual.transposarMatriuFigura();
+                        m_figura_actual.invertir(false, true);
+                    }
+                    else
+                    {
+                        m_figura_actual.transposarMatriuFigura();
+                        m_figura_actual.invertir(true, false);
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+    return si_girar;
 }
+
 
 
 bool Joc::mouFigura(int dirX)
 {
-    // To do
+    bool si_mou = true;
     return false;
 }
 
@@ -26,5 +92,16 @@ int Joc::baixaFigura()
 
 void Joc::escriuTauler(const string& nomFitxer)
 {
-    // To do
+    ofstream fitxer;
+    fitxer.open(nomFitxer);
+
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        for (int j = 0; j < MAX_COLUMNES; j++)
+        {
+            fitxer << m_tauler.getPixel(i, j);
+        }
+    }
+
+    fitxer.close();
 }
