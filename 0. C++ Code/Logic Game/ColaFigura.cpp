630af20d692b61e2d0d02987d3e002eeb35f2b68
Jan Krahl <jan@krahl.dev>
2024-06-01 17:28:41 +0200

    ColaFigura añadido

--- 0. C++ Code/Logic Game/ColaFigura.cpp
+++ 0. C++ Code/Logic Game/ColaFigura.cpp
@@ -1,2 +1,30 @@
 #include "ColaFigura.h"
 
+void CuaTipusFigura::afegeix(Figura& valor)
+{
+	NodeTipusFigura* aux = new NodeTipusFigura;
+	aux->m_valor = valor;
+	aux->m_next = nullptr;
+
+	if (esBuida()) {
+		m_primer = aux;
+		m_ultim = aux;
+	}
+	else {
+		m_ultim->m_next = aux;
+		m_ultim = aux;
+	}
+}
+
+void CuaTipusFigura::treu()
+{
+	if (esBuida())
+		return;
+
+	if (m_primer == m_ultim)
+		m_ultim = nullptr;
+
+	NodeTipusFigura* aux = m_primer;
+	m_primer = aux->m_next;
+	delete aux;
+}
--- 0. C++ Code/Logic Game/ColaFigura.h
+++ 0. C++ Code/Logic Game/ColaFigura.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "Figura.h"
+
+class NodeTipusFigura
+{
+public:
+	Figura m_valor;
+	NodeTipusFigura* m_next;
+};
+
+class CuaTipusFigura
+{
+public:
+	CuaTipusFigura() : m_primer(nullptr), m_ultim(nullptr) {}
+
+	void afegeix(Figura& valor);
+	void treu();
+	Figura& getPrimer() const { return m_primer->m_valor; }
+	Figura& getUltim() const { return m_ultim->m_valor; }
+	bool esBuida() const { return (m_primer == nullptr && m_ultim == nullptr); }
+private:
+	NodeTipusFigura* m_primer;
+	NodeTipusFigura* m_ultim;
+};
